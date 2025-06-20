c2ae3a5a79b3d797e5e4bd44bf2778c2a2a3d02a
ANISH-SR <rawatanish08@gmail.com>
2025-06-20 06:50:03 +0000

    deploy

--- app.py
+++ app.py
@@ -115,4 +115,10 @@ def index():
     return render_template('index.html', score=score, error=error)
 
 if __name__ == '__main__':
+    # Development
     app.run(debug=True)
+else:
+    # Production
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
